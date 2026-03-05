c3e507aafb52dcfeef4853d932e9cc6a11c3582b
HydroXCarbon <purin.pongpanich@gmail.com>
2026-03-05 17:08:07 +0700

    Refactor: Move landing page content to `app/pages/main/page.tsx` and update the root page to render it.

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,5 @@
-import React from 'react'
-import Hero from '@/app/components/Hero'
-import Roadmap from './components/Roadmap'
+import LandingPage from "@/app/pages/main/page";
 
-export default function LandingPage() {
-  return (
-   
-   <main className="flex min-h-screen flex-col">
-      <Hero />
-      
-      {/* วาง Roadmap ต่อจาก Hero ได้เลย */}
-      <Roadmap />
-      
-      {/* ส่วนอื่นๆ เช่น Products หรือ Support ที่คุณทำไว้ */}
-    </main>
-  )
+export default function Home() {
+  return <LandingPage />;
 }
---
+++ app/pages/main/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Hero from "@/app/pages/main/Hero";
+import Roadmap from "./Roadmap";
+
+export default function LandingPage() {
+  return (
+    <main className="flex min-h-screen flex-col">
+      <Hero />
+
+      {/* วาง Roadmap ต่อจาก Hero ได้เลย */}
+      <Roadmap />
+
+      {/* ส่วนอื่นๆ เช่น Products หรือ Support ที่คุณทำไว้ */}
+    </main>
+  );
+}
