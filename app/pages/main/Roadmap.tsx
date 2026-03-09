5140c51c00bc182f35131ae126ee2ef1e3a0c5cc
HydroXCarbon <purin.pongpanich@gmail.com>
2026-03-09 10:17:36 +0700

    Removes unused React imports

--- app/pages/main/Roadmap.tsx
+++ app/pages/main/Roadmap.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState } from "react";
+import { useState } from "react";
 import { motion, useTransform, useAnimationFrame, useMotionValue, } from 'framer-motion';
 
 const Roadmap = () => {
