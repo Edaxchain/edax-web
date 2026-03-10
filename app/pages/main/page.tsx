2ee8cf668c397290fb22b70314d0d24137d13897
EDAXINNOVATION <135825741+Edaxchain@users.noreply.github.com>
2026-03-10 07:58:25 +0700

    feat: Add about us page and partners section to main page (#3)
    Co-authored-by: AHang05 <148489862+AHang05@users.noreply.github.com>

--- app/pages/main/page.tsx
+++ app/pages/main/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Hero from '@/app/pages/main/Hero'
 import Roadmap from '@/app/pages/main/Roadmap'
+import Partners from '@/app/pages/about/Partners'
 
 export default function LandingPage() {
   return (
@@ -8,10 +9,10 @@ export default function LandingPage() {
    <main className="flex min-h-screen flex-col py-12">
       <Hero />
       
-      {/* วาง Roadmap ต่อจาก Hero ได้เลย */}
+      
       <Roadmap />
       
-      {/* ส่วนอื่นๆ เช่น Products หรือ Support ที่คุณทำไว้ */}
+      <Partners />
     </main>
   )
 }
