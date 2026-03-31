9ca6e8aabdfd37a5f76f5944c4730b2db2c30825
AHang05 <148489862+AHang05@users.noreply.github.com>
2026-03-31 08:49:24 +0700

    style: Change font

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,16 +4,26 @@ import { WalletContextProvider } from "@/app/contexts/WalletContextProvider";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
 import "./globals.css";
+import { Anuphan } from 'next/font/google'
+
+const anuphan = Anuphan({
+  weight: ['300', '400', '500', '600', '700'],
+  subsets: ['latin', 'thai'],
+  display: 'swap',
+  variable: '--font-anuphan',
+});
 
 export default async function RootLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
+
+
     const messages = await getMessages();
 
     return (
-        <html lang="en">
+        <html lang="en" className={anuphan.variable} suppressHydrationWarning>
             <body>
                 <NextIntlClientProvider messages={messages}>
                     <WalletContextProvider>
